36b36465c2a84eb41444a630480b023661328ef2
al385752 <101199100+al385752@users.noreply.github.com>
2022-10-14 19:33:21 +0200

    teleportGhost v3
    El fantasma se desplaza por encima del techo por una serie de "waypoints" con un tag, al llegar a uno, se agacha y sube al mismo punto.

--- Scripts/teleportBehaviour.cs
+++ Scripts/teleportBehaviour.cs
@@ -4,51 +4,74 @@ using UnityEngine;
 
 public class teleportBehaviour : MonoBehaviour
 {
-    public float velocidad = 10f;
-    //public Transform waypoint;
+    public float velocidad = 5f;
+    public float velocidadBajada = 10f;
     
     private ArrayList waypointsArray = new ArrayList();
     private Vector3 waypointActual;
+    private Vector3 waypointAnterior;
+    private Vector3 calculoBajada;
     private int waypointDestino;
     private bool andando = true;
+    private bool agachado = false;
 
     void Start(){
+
         GameObject[] waypointsLista = GameObject.FindGameObjectsWithTag("waypoint");
-        foreach(GameObject waypoint in waypointsLista){
+        foreach(GameObject waypoint in waypointsLista)
             waypointsArray.Add(waypoint.transform.position);
-        }
+        
+        calculoBajada = waypointActual - new Vector3(0, 15, 0);
         waypointActual = (Vector3) waypointsArray[0];
-        //StartCoroutine(CorutinaMovimiento(waypoint));
+
     }
 
     void Update(){
+
         if(Vector3.Distance(transform.position, waypointActual) < 2){
-            Debug.Log("He llegado al waypoint");
-            if(++waypointDestino == waypointsArray.Count){
+            if(++waypointDestino == waypointsArray.Count)
                 waypointDestino = 0;
-            }
+            waypointAnterior = waypointActual;
+            calculoBajada = waypointActual - new Vector3(0, 5, 0);
             waypointActual = (Vector3) waypointsArray[waypointDestino];
             andando = false;
         }
+
+
         if(andando)
             andarFuncion(waypointActual);
-        else
-            StartCoroutine(esperarYPaAbajoFuncion());
+        else if(!andando && !agachado)
+            StartCoroutine(agachadoFuncion());
+        else if(!andando && agachado)
+            StartCoroutine(subidaFuncion());
     }
 
     void andarFuncion(Vector3 waypoint){
+
         Vector3 direction = waypoint - transform.position;
         Vector3 movement = direction.normalized * velocidad * Time.deltaTime;
-        transform.LookAt(waypoint);
         transform.position = transform.position + movement;
-            //yield return null;
-        /*yield return new WaitForSeconds(3f);
-        print("La corutina de movimiento ha finalizado");
-        */
     }
 
-    IEnumerator esperarYPaAbajoFuncion(){
+    IEnumerator agachadoFuncion(){
+        
+        while(Vector3.Distance(transform.position, calculoBajada) > 0.05f){
+            transform.position = Vector3.Lerp(transform.position, calculoBajada, velocidadBajada * Time.deltaTime);
+            yield return null;
+        }
+        
+        yield return new WaitForSeconds(2.0f);
+        agachado = true;
+    }
+
+    IEnumerator subidaFuncion(){
+        while(Vector3.Distance(transform.position, waypointAnterior) > 0.05f){
+            transform.position = Vector3.Lerp(transform.position, waypointAnterior, velocidadBajada * Time.deltaTime);
+            yield return null;
+        }
+
         yield return new WaitForSeconds(2.0f);
         andando = true;
+        agachado = false;
     }
 }
