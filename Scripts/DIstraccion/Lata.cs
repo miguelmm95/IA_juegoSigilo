a62df346ce5bb0a5087c76aec7bccc5a0c2da87f
PKTxPepe <al385746@uji.es>
2022-10-20 22:21:14 +0200

    Laticosas
    Latinosé

--- Scripts/DIstraccion/Lata.cs
+++ Scripts/DIstraccion/Lata.cs
@@ -7,30 +7,51 @@ public class Lata : MonoBehaviour
     public CapsuleCollider capsule;
     //public AudioSource sonido;
     bool impacto = false;
+    //public StateManager state;
+    //public PatrolState patrol;
+    public Vector3 posicion;
 
     public Rigidbody can;
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody can = GetComponent<Rigidbody>();
-        Destroy(gameObject, 2);
+        Destroy(gameObject, 3);
+
     }
 
     // Update is called once per frame
     void OnCollisionEnter(Collision other) 
     {
-        Debug.Log(other.gameObject.tag);
         if(impacto == false)
         {
+            //guardar posición de la lata
+            posicion = this.transform.position;
             impacto = true;
             capsule.radius = 40;
-            Debug.Log(capsule.radius);
+            StartCoroutine(soundOff());
             //sonido.Play();
         }
         
         else
         {
             can.velocity.Set(0,0,0);
+        }
+
+    }
+
+    IEnumerator soundOff(){
+        yield return new WaitForSeconds(2);
+        capsule.radius = 1;
+    }
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.tag == "enemy"){
+            StateManager state = other.GetComponent<StateManager>();
+            Debug.Log(state.currentState);
+            PatrolState patrol = other.GetComponentInChildren<PatrolState>();
+            if(state.currentState is PatrolState){
+                patrol.goToInvestigate = true;
+            }
         }
 
     }
